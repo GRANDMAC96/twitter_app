033d692e9c8e42559535f9a6108d25997e5fcfd7
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-09 12:53:14 +0100

    tweet form now submits a tweet to tweets state

--- src/components/AddTweetForm.js
+++ src/components/AddTweetForm.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 class AddTweetForm extends React.Component {
+    tweetRef = React.createRef();
+
+
+    createTweet = e => {
+        e.preventDefault();
+        console.log(this.tweetRef.current.value);
+        const tweet = {
+            text: this.tweetRef.current.value,
+        }
+        this.props.addTweet(tweet);
+        // Finally refresh the page
+        // Here currentTarget refers to the form tag and the reset() method resets the form.
+        // NB* state resets when you refresh the page.
+        e.currentTarget.reset();
+    }
     render() {
         return (
-            <div>
-
-            </div>
+            <Form onSubmit={this.createTweet}>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Control as="textarea" rows={3} placeholder="Whats on your mind...?" ref={this.tweetRef} />
+                </Form.Group>
+                <Button variant="primary" type="submit">+ Add Tweet</Button>
+            </Form>
         );
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Header from "./Header";
+import AddTweetForm from "./AddTweetForm";
 
 class App extends React.Component {
+  state = {
+    tweets: {}
+  };
+
+  addTweet = tweet => {
+    // Options you may think would work to add things to state
+    // this.state.fishes.push(fish) for an array
+    // this.state.fishes.fish1 = fish for an object
+    // Neither work!!
+
+    // Instead you need to use setState()
+    // 1. take a copy of existing state
+    const tweets = { ...this.state.tweets };
+    // 2. add our new fish to the fishes variable
+    tweets[`tweet${Date.now()}`] = tweet;
+    // 3. set the new fishes object to state
+    this.setState({
+      tweets: tweets
+    });
+  };
+
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <AddTweetForm addTweet={this.addTweet} />
+      </div>
     );
   }
 }
