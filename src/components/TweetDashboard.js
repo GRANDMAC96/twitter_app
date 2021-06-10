aaefbdf0a0dc7d4c835a33a12c1a7e961b54b4c5
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-10 15:44:05 +0100

    tweet properties passed via app to tweetdashboard and then to tweet.js component via props

--- src/components/TweetDashboard.js
+++ src/components/TweetDashboard.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Tweet from './Tweet';
+
 
 class TweetDashboard extends React.Component {
     render() {
         return (
             <div>
-                elloe
+                {Object.keys(this.props.tweets).map(key => (
+                    <Tweet
+                        key={key}
+                        index={key}
+                        tweet={this.props.tweets[key]}
+                    />
+                ))}
+                <Button onClick={this.props.loadSampleTweets}>Load Sample Tweets</Button>
             </div>
+
         );
     }
 }
