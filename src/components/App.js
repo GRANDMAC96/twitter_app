aadeaf3cffe7f53f011dda1750d9810dc998bccb
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-10 11:59:51 +0100

    tweetdashboard added and imported to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import AddTweetForm from "./AddTweetForm";
+import TweetDashboard from "./TweetDashboard";
 
 class App extends React.Component {
   state = {
@@ -29,6 +30,7 @@ class App extends React.Component {
       <div>
         <Header />
         <AddTweetForm addTweet={this.addTweet} />
+        <TweetDashboard />
       </div>
     );
   }
---
+++ src/components/TweetDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TweetDashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                elloe
+            </div>
+        );
+    }
+}
+
+export default TweetDashboard;
