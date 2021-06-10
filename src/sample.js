5b7a7c9aa14fb3bad5c670219ab851c2d8817fcc
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-10 12:22:36 +0100

    created sample tweets component

---
+++ src/sample.js
@@ -0,0 +1,39 @@
+const tweets = {
+    fish1: {
+        tweet: "Oh Donald"
+    },
+
+    fish2: {
+        tweet: "Yolo"
+    },
+
+    fish3: {
+        tweet: "Nothing to see here"
+    },
+
+    fish4: {
+        tweet: "Tweet tweet"
+    },
+
+    fish5: {
+        tweet: "Funk the beat"
+    },
+
+    fish6: {
+        tweet: "Hold your breath"
+    },
+
+    fish7: {
+        tweet: "Why you do this"
+    },
+
+    fish8: {
+        tweet: "Ha ha ha ha ha"
+    },
+
+    fish9: {
+        tweet: "You'll never walk alone"
+    }
+};
+
+export default tweets;
