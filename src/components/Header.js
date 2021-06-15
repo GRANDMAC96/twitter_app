ec767cecca6d0bc6e93357a2e212be2952331c7d
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-15 10:43:49 +0100

    a few deletions

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,13 @@ import Container from 'react-bootstrap/Container';
 class Header extends React.Component {
     render() {
         return (
-            <div className="row">
-                <h2 className="col-3 text-left">Home</h2>
-                <h3 className="col-6 text-center">Profile</h3>
-                <h2 className="col-3 text-right">News</h2>
-            </div>
+            <>
+                <div className="row">
+                    <h2 className="col-3 text-left">Home</h2>
+                    <h3 className="col-6 text-center">Profile</h3>
+                    <h2 className="col-3 text-right">News</h2>
+                </div>
+            </>
         );
     }
 }
